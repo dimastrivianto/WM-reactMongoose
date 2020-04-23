b8c1aa8ff239f81d09eb4b070b3341a24fcd2be4
dimtriv <dimastrivianto@gmail.com>
2020-04-23 12:17:37 +0700

    redeploy

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 9980
+const path = require('path')
+
+//__dirname, letak server berada
+app.use(express.static(__dirname))
+//__dirname + build, letak folder build berada
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(port, () => console.log('Running in production'))
